9b4278c24ee72f5a34a501adbd92b09a80dd4f53
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-06 12:02:31 +0800

    test

--- ____tests____/index.test.js
+++ ____tests____/index.test.js
@@ -4,4 +4,11 @@ describe('describe block', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  test('adds 2 + 2 to equal 4', done => {
+    setTimeout(() => {
+      expect(sum(2, 2)).toBe(4);
+      done();
+    });
+  });
 });
